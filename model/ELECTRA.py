103abaf317060a5b2b5eae72842bdf5cdb3d0e27
Bill845514379 <51468896+Bill845514379@users.noreply.github.com>
2022-04-17 15:17:43 +0800

    Add files via upload

---
+++ model/ELECTRA.py
@@ -0,0 +1,18 @@
+
+import torch.nn as nn
+from config.cfg import path, cfg
+from transformers import ElectraModel, ElectraForSequenceClassification
+
+class ELECTRA(nn.Module):
+
+    def __init__(self):
+        super(ELECTRA, self).__init__()
+        self.electra = ElectraModel.from_pretrained(path['electra_path'])
+        self.dropout = nn.Dropout(cfg['dropout'])
+        self.fc = nn.Linear(cfg['hidden_dim'], 2)
+
+    def forward(self, input_x, token_type_ids, attention_mask):
+        x = self.electra(input_x, token_type_ids, attention_mask)
+        x = self.dropout(x)
+        x = self.fc(x)
+        return x
