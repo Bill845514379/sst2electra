e4a3a8afc38a82a05579559edfec329f57231459
Bill845514379 <51468896+Bill845514379@users.noreply.github.com>
2022-04-17 17:06:23 +0800

    Update load_data.py

--- common/load_data.py
+++ common/load_data.py
@@ -1,11 +1,11 @@
 
 from datasets import load_dataset
 from config.cfg import cfg, path
-from transformers import ElectraTokenizer, ElectraModel
+from transformers import ElectraTokenizer, ElectraModel, BertTokenizer
 import numpy as np
 import torch
 
-tokenizer = ElectraTokenizer.from_pretrained(path['electra_path'], do_lower_case=True)
+tokenizer = BertTokenizer.from_pretrained(path['electra_path'], do_lower_case=True)
 
 def tran_list(data):
   t = []
@@ -21,8 +21,6 @@ def load_data(path):
     data_train = sst2['train']
     data_val = sst2['validation']
     data_test = sst2['test']
-    print(data_train['sentence'][0])
-    print(data_train['tokens'][0])
     x_train = tokenizer(tran_list(data_train['sentence']), padding=True, return_tensors="pt")
     y_train = torch.tensor(data_train['label'])
 
