264fe3362b8f28bbf834ef6b53e43ae1df09130b
Bill845514379 <51468896+Bill845514379@users.noreply.github.com>
2022-04-17 16:22:32 +0800

    Update load_data.py

--- common/load_data.py
+++ common/load_data.py
@@ -5,7 +5,7 @@ from transformers import ElectraTokenizer, ElectraModel
 import numpy as np
 import torch
 
-tokenizer = ElectraTokenizer.from_pretrained(path['electra_path'])
+tokenizer = ElectraTokenizer.from_pretrained(path['electra_path'], do_lower_case=True)
 
 def tran_list(data):
   t = []
@@ -23,13 +23,13 @@ def load_data(path):
     data_test = sst2['test']
     print(data_train['sentence'][0])
     print(data_train['tokens'][0])
-    x_train = tokenizer(tran_list(data_train['sentence']), padding=True, return_tensors="pt", do_lower_case=True)
+    x_train = tokenizer(tran_list(data_train['sentence']), padding=True, return_tensors="pt")
     y_train = torch.tensor(data_train['label'])
 
-    x_val = tokenizer(tran_list(data_val['sentence']),padding=True, return_tensors="pt", do_lower_case=True)
+    x_val = tokenizer(tran_list(data_val['sentence']),padding=True, return_tensors="pt")
     y_val = torch.tensor(data_val['label'])
 
-    x_test = tokenizer(tran_list(data_test['sentence']) ,padding=True, return_tensors="pt", do_lower_case=True)
+    x_test = tokenizer(tran_list(data_test['sentence']) ,padding=True, return_tensors="pt")
     y_test = torch.tensor(data_test['label'])
 
     return x_train, y_train, x_val, y_val, x_test, y_test
