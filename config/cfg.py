2dcb880e931801e61367383ed5e06122cd2b997d
Bill845514379 <51468896+Bill845514379@users.noreply.github.com>
2022-04-17 14:10:33 +0800

    Update cfg.py

--- config/cfg.py
+++ config/cfg.py
@@ -3,7 +3,7 @@
 
 path = {
     'data_path': 'data/sst2/',
-    'electra_path': 'bhadresh-savani/electra-base-discriminator'
+    'electra_path': 'bhadresh-savani/electra-base-emotion'
 }
 
 cfg = {
