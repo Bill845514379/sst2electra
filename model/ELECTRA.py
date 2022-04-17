d41ae8c939a5db197bd8e3709e78479693dafaf3
Bill845514379 <51468896+Bill845514379@users.noreply.github.com>
2022-04-17 15:38:41 +0800

    Update ELECTRA.py

--- model/ELECTRA.py
+++ model/ELECTRA.py
@@ -14,7 +14,7 @@ class ELECTRA(nn.Module):
     def forward(self, input_ids, token_type_ids, attention_mask):
         x = self.electra(input_ids, token_type_ids, attention_mask)
         x = x[0]
-        x = x[:][0]
+        x = x[:, 0, :]
         print(x.shape)
         x = self.dropout(x)
         x = self.fc(x)
