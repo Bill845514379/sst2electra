83c423eba7b31690c6cfae5ae2875c1e183c9b4c
Bill845514379 <51468896+Bill845514379@users.noreply.github.com>
2022-04-17 14:08:27 +0800

    Update cfg.py

--- config/cfg.py
+++ config/cfg.py
@@ -3,17 +3,11 @@
 
 path = {
     'data_path': 'data/sst2/',
-    'electra_path': 'google/electra-base-discriminator'
+    'electra_path': 'bhadresh-savani/electra-base-discriminator'
 }
 
 cfg = {
     'batch_size': 16,
     'epoch': 10,
     'learning_rate': 1e-5,
-}
-
-hyper = {
-    'lstm_hidden': 256,
-    'word_dim': 300,
-    'dropout': 0.5
 }
