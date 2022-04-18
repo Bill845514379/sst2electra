a456a5dedf58fb8087bb6727c1f881fcc7f8ddc8
Bill845514379 <51468896+Bill845514379@users.noreply.github.com>
2022-04-18 15:24:00 +0800

    Update cfg.py

--- config/cfg.py
+++ config/cfg.py
@@ -5,14 +5,16 @@ path = {
     'data_path': 'data/sst2/',
     # 'electra_path': 'bhadresh-savani/electra-base-emotion',
     # 'electra_path': 'pretrained_model/electra-base-discriminator',
-    'electra_path': 'google/electra-base-discriminator'
+    'electra_path': 'google/electra-small-discriminator',
+    'bert_path': 'bert-base-cased'
     # 'electra_path': 'bert-base-cased',
 }
 
 cfg = {
-    'hidden_dim': 768,
+    'hidden_dim': 256,
     'dropout': 0.2,
     'batch_size': 16,
     'epoch': 10,
     'learning_rate': 1e-4,
+    'electra_flag': True,  # False 代表使用Bert base cased模型
 }
