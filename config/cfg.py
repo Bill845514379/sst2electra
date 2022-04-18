5b1ee9d64f14d37a56515aca6e900c8ea53234de
Bill845514379 <51468896+Bill845514379@users.noreply.github.com>
2022-04-18 14:55:02 +0800

    Update cfg.py

--- config/cfg.py
+++ config/cfg.py
@@ -5,13 +5,14 @@ path = {
     'data_path': 'data/sst2/',
     # 'electra_path': 'bhadresh-savani/electra-base-emotion',
     # 'electra_path': 'pretrained_model/electra-base-discriminator',
-    'electra_path': 'bert-base-cased',
+    'electra_path': 'google/electra-small-discriminator'
+    # 'electra_path': 'bert-base-cased',
 }
 
 cfg = {
     'hidden_dim': 768,
     'dropout': 0.2,
     'batch_size': 16,
-    'epoch': 10,
-    'learning_rate': 2e-5,
+    'epoch': 30,
+    'learning_rate': 1e-4,
 }
